6b89237f3fd2c5cd7a67d5cbb1932257f08a8404
ArunGovil <a.arungovil@gmail.com>
2022-11-17 14:53:10 +0530

    update/welcome-image

--- src/data/OnBoarding.ts
+++ src/data/OnBoarding.ts
@@ -3,24 +3,24 @@ export const OnBoarding = [
     id: 1,
     title: 'Title One',
     description: 'Description One',
-    icon: require('../assets/welcome.jpeg'),
+    icon: require('../assets/welcome.jpg'),
   },
   {
     id: 2,
     title: 'Title Two',
     description: 'Description Two',
-    icon: require('../assets/welcome.jpeg'),
+    icon: require('../assets/welcome.jpg'),
   },
   {
     id: 3,
     title: 'Title Three',
     description: 'Description Two',
-    icon: require('../assets/welcome.jpeg'),
+    icon: require('../assets/welcome.jpg'),
   },
   {
     id: 4,
     title: 'Title Four',
     description: 'Description Two',
-    icon: require('../assets/welcome.jpeg'),
+    icon: require('../assets/welcome.jpg'),
   },
 ];
