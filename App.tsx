15ace8abb8348c90c53fe315139df88022696ba2
ArunGovil <a.arungovil@gmail.com>
2022-12-14 14:20:38 +0530

    branch/testing

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import {View} from 'react-native';
 import React from 'react';
-import FlatBoard from './src/screens/FlatBoard';
 import {OnBoarding} from './src/data/OnBoarding';
+import FlatBoard from 'react-native-flatboard';
 
 const onComplete = () => {
   console.log('Done');
@@ -14,7 +14,7 @@ export default function App() {
         data={OnBoarding}
         onFinish={onComplete}
         accentColor="#93c01f"
-        buttonTitle="Lets Go"
+        buttonTitle="Test Go!"
       />
     </View>
   );
