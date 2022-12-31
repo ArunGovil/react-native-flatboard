2d057c447571024e6c282cf4414ae97680b1422d
ArunGovil <a.arungovil@gmail.com>
2022-12-31 14:41:41 +0530

    fix/types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,14 @@
 export type OnBoardingData = {
-        id: number;
-        title: string;
-        description: string;
-        icon: string;
+  id: number;
+  title: string;
+  description: string;
+  icon: string;
 };
 
 export type TextStyles = {
-        fontSize?: number;
-        fontFamily?: string;
-        color?: string;
-        fontWeight?: string | number;
-        textAlign?: string;
+  fontSize?: number;
+  fontFamily?: string;
+  color?: string;
+  fontWeight?: string | number;
+  textAlign?: string;
 };
